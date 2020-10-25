bc98c913510a71de797049960bbbc112742fc6f9
vapvin <vins@vins.dev>
2020-10-25 16:12:04 +0900

    Stack

--- stack.js
+++ stack.js
@@ -1,6 +1,27 @@
 class Stack {
     constructor () {
-      
+      this.store = [];
     }
 
+    push(data){
+        this.store.push(data);
+    }
+
+    pop(){
+        return this.store.pop();
+    }
+
+    peek(){
+        return this.store[this.store.length-1];
+    }
 }
+
+const stack = new Stack();
+
+stack.push(2);
+stack.push(4);
+stack.push(6);
+stack.push(8);
+stack.pop();
+const stackPeek = stack.peek();
+console.log(stack, stackPeek);
