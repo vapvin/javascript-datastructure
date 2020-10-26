e0deb3b96167e0ee1ed20ac4a0a95ebcbce68273
vapvin <vins@vins.dev>
2020-10-26 20:45:01 +0900

    Graph?

--- graph.js
+++ graph.js
@@ -20,4 +20,34 @@ class Graph {
             delete this.nodes[node];
         }
     }
+
+    hasEdge(fromNode, toNode){
+        if(!this.contains(fromNode)){
+            return false;
+        }
+        return !!this.nodes[fromNode].edges[toNode];
+    }
+
+    addEdge(fromNode, toNode){
+        if(!this.contains(fromNode) || !this.contains(toNode)){
+            return false;
+        }
+        this.nodes[fromNode].edges[toNode] = toNode;
+        this.nodes[toNode].edges[fromNode] = fromNode;
+    }
+
+    removeEdge(fromNode, toNode) {
+        if(!this.contains(fromNode) || !this.contains(toNode)){
+            return false;
+        }
+        delete this.nodes[fromNode].edges[toNode];
+        delete this.nodes[toNode].edges[fromNode];
+    }
+
+    eachNode(func){
+        const keys = Object.keys(this.nodes);
+        for(i = 0; i < keys.length; i++){
+            func(keys[i]);
+        }
+    }
 }
