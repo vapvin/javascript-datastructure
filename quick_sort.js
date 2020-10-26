66760ffb6ba4daf4df2b498ace43fea693b2065d
vapvin <vins@vins.dev>
2020-10-26 21:04:15 +0900

    unstable quick sort

--- quick_sort.js
+++ quick_sort.js
@@ -27,3 +27,42 @@ const quickSortStable = arr => {
 const sortArray = quickSortStable([9,5,4,2,8,6,1,3,7]);
 console.log(sortArray);
 
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
+    if(left >= right){
+        return;
+    }
+
+    const mid = Math.floor((left + right) / 2);
+    const pivot = arr[mid];
+   
+
+    const divide = (arr, left, right, pivot) => {
+        while(left <= right){
+            while(arr[left] < pivot){
+                left++;
+            }
+            while(arr[right] > pivot){
+                right--;
+            }
+
+            if(left <= right){
+                let swap = arr[left];
+                arr[left] = arr[right];
+                arr[right] = swap;
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+    const partition = divide(arr, left, right, pivot);;
+
+    quickSort(arr, left, partition - 1);
+    quickSort(arr, partition, right);
+
+    return arr;
+
+}
+
+const result = quickSort([3,2,7,5,4,1]);
+console.log(result);
