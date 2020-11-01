38d02725482cb980b38e5262c8cff79e2fa98990
vapvin <vins@vins.dev>
2020-11-01 21:37:02 +0900

    star

---
+++ star.js
@@ -0,0 +1,25 @@
+// const number = 50;
+
+// let star = "";
+
+// for(i = 1; i <= number; i++){
+
+//     for(j = 1; j <= number - i; j++){
+//         star += " ";
+//     }
+
+//     for(k = 1; k <= 2*i-1; k++){
+//         star += "*"
+//     }
+//     star += "\n";
+// }
+
+// console.log(star)
+
+// let s = 0;
+
+// for(i = 1; i <= 100; i++){
+//     s += i;
+// }
+
+// console.log(s)
